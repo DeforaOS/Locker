88bd35bdc71bcf5abdaeca1d38918e7acc448ddc
Pierre Pronchery <khorben@defora.org>
2014-06-23 00:30:14 +0200

    Consistency fix for the usage screen

--- src/lockerctl.c
+++ src/lockerctl.c
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2011-2012 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2011-2014 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS Desktop Locker */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -54,7 +54,7 @@ static int _lockerctl(int action)
 static int _usage(void)
 {
 #ifdef EMBEDDED
-	fputs(_("Usage: lockerctl [-D | -E | -S | -l | -s | -u | -z]\n"
+	fputs(_("Usage: lockerctl [-D|-E|-S|-l|-s|-u|-z]\n"
 "  -D	Temporarily disable the screensaver\n"
 "  -E	Enable the screensaver again\n"
 "  -S	Display or change settings\n"
