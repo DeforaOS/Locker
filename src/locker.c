4a519f6933146eb264018864d118679222825922
Pierre Pronchery <khorben@defora.org>
2018-10-19 20:45:59 +0200

    Update the copyright notice

--- src/locker.c
+++ src/locker.c
@@ -1,6 +1,6 @@
 /* $Id$ */
 static char const _copyright[] =
-"Copyright © 2010-2015 Pierre Pronchery <khorben@defora.org>";
+"Copyright © 2010-2018 Pierre Pronchery <khorben@defora.org>";
 /* This file is part of DeforaOS Desktop Locker */
 static char const _license[] =
 "This program is free software: you can redistribute it and/or modify\n"
